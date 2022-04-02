8ecad0ee757ba9a5508150423f38c1472d0b2381
Markus Haslinger <markus@marce.at>
2022-04-02 20:53:37 +0200

    map endpoints

--- Endpoints.cs
+++ Endpoints.cs
@@ -1,9 +1,66 @@
-﻿namespace MinimalBackend;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalBackend;
 
 public static class Endpoints
 {
     public static void ConfigureEndpoints(this WebApplication app)
     {
-        app.MapGet("/", () => "Hello World!");
+        app.MapGet("/demo/foo", () => "bar");
+        app.MapGet("/demo/add", (int num1, int? num2) => $"Your result: {num1 + num2 ?? 0}");
+
+        ConfigureProductEndpoints(app);
+        ConfigureMenuEndpoints(app);
+    }
+
+    private static void ConfigureMenuEndpoints(IEndpointRouteBuilder app)
+    {
+        static IQueryable<Menu> GetMenus(DataContext ctx) => ctx.Menus
+            .Include(m => m.Products)
+            .ThenInclude(p => p.Product);
+
+        app.MapGet("/menu", async (DataContext ctx) => await GetMenus(ctx).ToListAsync());
+        app.MapGet("/menu/{no}/total-price", async (DataContext ctx, int no) =>
+        {
+            return await GetMenus(ctx).Where(m => m.MenuNo == no)
+                .SelectMany(m => m.Products)
+                .Select(mi => mi.Amount * mi.Product.Price)
+                .FirstOrDefaultAsync();
+        });
+
+        app.MapDelete("/menu/{no}", async (DataContext ctx, int no) =>
+        {
+            var menu = await ctx.Menus.FirstOrDefaultAsync(m => m.MenuNo == no);
+            if (menu == null)
+            {
+                return;
+            }
+
+            ctx.Menus.Remove(menu);
+            await ctx.SaveChangesAsync();
+        });
+    }
+
+    private static void ConfigureProductEndpoints(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/product", async (DataContext ctx) => await ctx.Products.ToListAsync());
+        app.MapGet("/product/{id}", async (DataContext ctx, int id) =>
+            await ctx.Products.FirstOrDefaultAsync(p => p.Id == id));
+        app.MapGet("/product/type", async (DataContext ctx, ProductTypes type) =>
+            await ctx.Products.Where(p => p.Type == type).ToListAsync());
+
+        app.MapPost("/product", async (DataContext ctx, NewProduct newProduct) =>
+        {
+            var newEntity = new Product
+            {
+                Price = newProduct.Price,
+                Hot = newProduct.Hot,
+                Name = newProduct.Name,
+                Type = newProduct.Type,
+                Vegetarian = newProduct.Vegetarian
+            };
+            await ctx.AddAsync(newEntity);
+            await ctx.SaveChangesAsync();
+        });
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
 using MinimalBackend;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
+builder.Services.Configure<JsonOptions>(o => o.SerializerOptions.Converters
+    .Add(new JsonStringEnumConverter()));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await context.Database.EnsureDeletedAsync();
+    await context.Database.MigrateAsync();
+    await context.InsertSampleData();
+}
+
 app.UseSwagger();
-
 app.ConfigureEndpoints();
-
 app.UseSwaggerUI();
 app.Run();
